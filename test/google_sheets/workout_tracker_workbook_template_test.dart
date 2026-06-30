1fa13f8bedfdb6415b429cadd09e0985800414eb
spielman <ian.b.spielman@gmail.com>
2026-06-30 14:56:18 -0400

    Consolidate equivalent default exercises

--- test/google_sheets/workout_tracker_workbook_template_test.dart
+++ test/google_sheets/workout_tracker_workbook_template_test.dart
@@ -20,10 +20,12 @@ void main() {
       expect(rowsByName, contains('Cable Dorsiflexion'));
       expect(rowsByName, contains('Bench Row'));
       expect(rowsByName, contains('Squat'));
-      expect(rowsByName, contains('DB Bench Press'));
+      expect(rowsByName, contains('DB Flat Bench Press'));
       expect(rowsByName, contains('Box Jump'));
       expect(rowsByName, contains('Dips'));
       expect(rowsByName, contains('Side Plank'));
+      expect(rowsByName, isNot(contains('Deep Squat')));
+      expect(rowsByName, isNot(contains('DB Bench Press')));
 
       expect(rowsByName['Smith Machine Reverse Lunge'], [
         'Smith Machine Reverse Lunge',
@@ -66,8 +68,19 @@ void main() {
         '8',
         '3m',
         '3-1-1',
-        'Fall program squat; note says to switch to deep squat on week 11.',
+        'Squat pattern from fall and winter programs.',
         '{Weight}[x]{Reps}[@]{RPE}[,]{Pain}',
+      ]);
+      expect(rowsByName['DB Flat Bench Press'], [
+        'DB Flat Bench Press',
+        'Dumbbell flat bench press',
+        '3',
+        '8-12',
+        '8',
+        '90s',
+        '',
+        'Tuck elbows to 45 degrees and press in a slight arc.',
+        '{Weight}[x]{Reps}[@]{RPE}',
       ]);
       expect(rowsByName['Dips']?[8], '{Reps}[@]{RPE}');
       expect(rowsByName['Side Plank']?[8], '{Seconds}[s@]{RPE}');
