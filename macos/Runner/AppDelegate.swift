6c3a0bbe07e9e15f8742345e8221f90b11226357
spielman <ian.b.spielman@gmail.com>
2026-06-30 10:55:24 -0400

    Focus existing macOS window on app links

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -5,7 +5,20 @@ import FlutterMacOS
 class AppDelegate: FlutterAppDelegate {
   override func applicationDidFinishLaunching(_ notification: Notification) {
     super.applicationDidFinishLaunching(notification)
-    mainFlutterWindow?.makeKeyAndOrderFront(nil)
+    focusMainFlutterWindow()
+  }
+
+  override func applicationShouldOpenUntitledFile(_ sender: NSApplication) -> Bool {
+    focusMainFlutterWindow()
+    return false
+  }
+
+  override func applicationShouldHandleReopen(
+    _ sender: NSApplication,
+    hasVisibleWindows flag: Bool
+  ) -> Bool {
+    focusMainFlutterWindow()
+    return false
   }
 
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
@@ -14,5 +27,10 @@ class AppDelegate: FlutterAppDelegate {
 
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
+  }
+
+  private func focusMainFlutterWindow() {
+    mainFlutterWindow?.makeKeyAndOrderFront(nil)
+    NSApp.activate(ignoringOtherApps: true)
   }
 }
